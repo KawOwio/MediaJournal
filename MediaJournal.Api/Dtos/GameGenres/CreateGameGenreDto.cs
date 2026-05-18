aeb7fdff624e11461032e97cfe28be44c0c61dfc
Nikita <n.gribuska@gmail.com>
2026-05-18 21:40:44 +0300

    Renamed AddGameGenreDto to CreateGameGenreDto

--- MediaJournal.Api/Dtos/GameGenres/CreateGameGenreDto.cs
+++ MediaJournal.Api/Dtos/GameGenres/CreateGameGenreDto.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace MediaJournal.Api.Dtos.GameGenres;
 
-public record  AddGameGenreDto(
+public record  CreateGameGenreDto(
     [Length(3, 20, ErrorMessage = "Genre has to be 3-20 characters long")]
     string Name
 );
